f56d562f148070edd60ef4e75c3c553d77c92092
Rhuan-Zucarelli <rhuan1zucarelli@gmail.com>
2023-06-21 13:57:52 -0300

    criando flutter + widgets

---
+++ Flutter/aluno_app_flutter/lib/iu/pages/pageInfoAluno.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageInfoAluno extends StatefulWidget {
+  const PageInfoAluno({super.key});
+
+  @override
+  State<PageInfoAluno> createState() => PageInfoAlunosState();
+}
+
+class PageInfoAlunosState extends State<PageInfoAluno> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
