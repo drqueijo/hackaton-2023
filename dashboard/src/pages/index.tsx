502219c20da54e6d9e0e9960b35265c77a4510eb
matheus <silveriomat1234@gmail.com>
2023-06-23 16:18:41 -0300

    feat: add auth with discord

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -1,5 +1,24 @@
-import BooksList from "./books";
-
+import Button from "n/components/UI/Button";
+import { api } from "n/utils/api";
+import { signIn, signOut, useSession } from "next-auth/react";
 export default function Home() {
-  return <BooksList />
+  const { data: sessionData } = useSession();
+  
+  const { data: secretMessage } = api.auth.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-gray-800">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <Button
+        text={sessionData ? "Sign out" : "Sign in"}
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      />
+    </div>
+  );
 }
---
+++ dashboard/src/server/api/routers/auth.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "n/server/api/trpc";
+
+export const authRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
