818c675f47c03910c9dfa2842e760c75d48d15ac
Rhuan-Zucarelli <rhuan1zucarelli@gmail.com>
2023-06-26 10:50:07 -0300

    subindo pra main

--- app_flutter/lib/models/livros.dart
+++ app_flutter/lib/models/livros.dart
@@ -1,16 +1,29 @@
-class Livro{
+import 'package:app_flutter/models/autor.dart';
+import 'package:app_flutter/models/editora.dart';
+
+class Livro {
   int id;
   String title;
   String subtitle;
-  int autorId; // Adicione esse campo
-  int editoraId; // Adicione esse campo
+  String isbn;
+  String place;
+  int year;
+  int publisherId;
+  int authorId;
+  Autor author;
+  Editora publisher;
 
   Livro({
     required this.id,
     required this.title,
     required this.subtitle,
-    required this.autorId,
-    required this.editoraId,
+    required this.isbn,
+    required this.place,
+    required this.year,
+    required this.publisherId,
+    required this.authorId,
+    required this.author,
+    required this.publisher,
   });
 
   factory Livro.fromJson(Map<String, dynamic> json) {
@@ -18,8 +31,13 @@ class Livro{
       id: json['id'],
       title: json['title'],
       subtitle: json['subtitle'],
-      autorId: json['autorId'],
-      editoraId: json['editoraId'],
+      isbn: json['isbn'],
+      place: json['place'],
+      year: json['year'],
+      publisherId: json['publisher_id'],
+      authorId: json['author_id'],
+      author: Autor.fromJson(json['author']),
+      publisher: Editora.fromJson(json['publisher']),
     );
   }
 }
--- app_flutter/lib/ui/pages/detalhes_livros.dart
+++ app_flutter/lib/ui/pages/detalhes_livros.dart
@@ -1,45 +1,10 @@
 import 'package:app_flutter/models/livros.dart';
-import 'package:app_flutter/models/autor.dart';
-import 'package:app_flutter/models/editora.dart';
-import 'package:app_flutter/ui/api/api.dart';
 import 'package:flutter/material.dart';
 
-class DetalhesLivro extends StatefulWidget {
+class DetalhesLivro extends StatelessWidget {
   final Livro livro;
 
   DetalhesLivro({required this.livro});
-
-  @override
-  _DetalhesLivroState createState() => _DetalhesLivroState();
-}
-
-class _DetalhesLivroState extends State<DetalhesLivro> {
-  late Future<Autor> autorFuture;
-  late Future<Editora> editoraFuture;
-  late Future<Livro> livroFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    livroFuture = fetchLivro() as Future<Livro>;
-    autorFuture = fetchAutor();
-    editoraFuture = fetchEditora();
-  }
-
-  Future<List<Livro>> fetchLivro() async {
-  final api = ApiRemote();
-  return api.getLivros();
-  }
-
-  Future<Autor> fetchAutor() async {
-    final api = ApiRemote();
-    return api.getAutorById(widget.livro.autorId);
-  }
-
-  Future<Editora> fetchEditora() async {
-    final api = ApiRemote();
-    return api.getEditoraById(widget.livro.editoraId);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,100 +14,62 @@ class _DetalhesLivroState extends State<DetalhesLivro> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
+        child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Título: ${widget.livro.title}',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Subtítulo: ${widget.livro.subtitle}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'ID: ${widget.livro.id}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 20),
-            FutureBuilder<Autor>(
-              future: autorFuture,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final autor = snapshot.data!;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Autor: ${autor.name}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Endereço do Autor: ${autor.address}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Cidade do Autor: ${autor.city}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'UF do Autor: ${autor.uf}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('Erro ao carregar autor');
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
-            ),
-            SizedBox(height: 20),
-            FutureBuilder<Editora>(
-              future: editoraFuture,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final editora = snapshot.data!;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Editora: ${editora.name}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Endereço da Editora: ${editora.address}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Cidade da Editora: ${editora.city}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        'UF da Editora: ${editora.uf}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('Erro ao carregar editora');
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildDetalhe('ID:', livro.id.toString(), 20, FontWeight.bold),
+                    _buildDetalhe('Título:', livro.title, 20, FontWeight.bold),
+                    _buildDetalhe('Subtítulo:', livro.subtitle, 20, FontWeight.bold),
+                    _buildDetalhe('ISBN:', livro.isbn, 20, FontWeight.bold),
+                    _buildDetalhe('Local:', livro.place, 20, FontWeight.bold),
+                    _buildDetalhe('Ano:', livro.year.toString(), 20, FontWeight.bold),
+                    SizedBox(height: 10),
+                    _buildDetalhe('Autor:', '', 20, FontWeight.bold),
+                    _buildDetalhe('ID:', livro.author.id.toString(), 16, FontWeight.normal),
+                    _buildDetalhe('Nome:', livro.author.name, 16, FontWeight.normal),
+                    _buildDetalhe('Endereço:', livro.author.address, 16, FontWeight.normal),
+                    _buildDetalhe('Cidade:', livro.author.city, 16, FontWeight.normal),
+                    _buildDetalhe('UF:', livro.author.uf, 16, FontWeight.normal),
+                    _buildDetalhe('Telefone:', livro.author.phone.toString(), 16, FontWeight.normal),
+                    SizedBox(height: 10),
+                    _buildDetalhe('Editora:', '', 20, FontWeight.bold),
+                    _buildDetalhe('ID:', livro.publisher.id.toString(), 16, FontWeight.normal),
+                    _buildDetalhe('Nome:', livro.publisher.name, 16, FontWeight.normal),
+                    _buildDetalhe('Endereço:', livro.publisher.address, 16, FontWeight.normal),
+                    _buildDetalhe('Cidade:', livro.publisher.city, 16, FontWeight.normal),
+                    _buildDetalhe('UF:', livro.publisher.uf, 16, FontWeight.normal),
+                    _buildDetalhe('Telefone:', livro.publisher.phone.toString(), 16, FontWeight.normal),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildDetalhe(String label, String value, double fontSize, FontWeight fontWeight) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+        ),
+        SizedBox(width: 5),
+        Text(
+          value,
+          style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+        ),
+        SizedBox(height: 10),
+      ],
     );
   }
 }
